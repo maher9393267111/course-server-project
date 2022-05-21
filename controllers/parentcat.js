92e8fbbd36aefc52d1ede650e1ed5a75afaec700
maher <gomemahero.com>
2022-05-21 13:31:33 +0300

    delete oldimage and parent category is work

--- controllers/parentcat.js
+++ controllers/parentcat.js
@@ -1,48 +1,106 @@
-const parentCatModel = require('../models/parentcategory');
+const parentCatModel = require("../models/parentcategory");
+const {cloudinary2} = require("../controllers/cloudinary");
 
 // cereate parent category
 
-
 exports.createParentCat = async (req, res) => {
+  const { name, image } = req.body;
+  console.log(req.body);
 
+  const newParentCat = new parentCatModel({
+    name,
+    image,
+  });
 
-    const { name, image } = req.body;
-    console.log(req.body);
+  const parentcat = await newParentCat.save();
+  res.status(200).json({
+    msg: "parent category created successfully",
+    parentcat,
+  });
 
-    const newParentCat = new parentCatModel({
-        name,
-        image,
+  // handle error
 
-
+  if (!parentcat) {
+    res.status(400).json({
+      mesage: "error creating parent category",
     });
-
-    const parentcat = await newParentCat.save();
-    res.status(200).json({
-        msg: "parent category created successfully",
-        parentcat,
-    });
-
-    // handle error
-
-    if (!parentcat) {
-        res.status(400).json({
-            mesage: "error creating parent category",
-        });
-    }
-
-}
-
-
+  }
+};
 
 // all parent categories
 
-
 exports.allParentCats = async (req, res) => {
+  const parentcats = await parentCatModel.find();
+  res.status(200).json({
+    message: "all parent categories",
+    parentcats,
+  });
+};
+
+
+
+// update parent category
+
+ exports.updateParentCat = async (req, res) => {
+    const { name, image } = req.body;
+    console.log(req.body);
+
+    try{
+
     
-        const parentcats = await parentCatModel.find();
+    console.log(req.body);
+    const parentcat = await parentCatModel.findByIdAndUpdate(
+        req.params.id,
+        {
+
+          $set:req.body,
+        // name,
+        // image,
+        },
+        { new: true }
+    );
+    res.status(200).json({
+        message: "parent category updated successfully",
+        parentcat,
+    });
+}
+catch(err){
+    console.log(err);
+    res.status(400).json({
+        errmsg:err,
+        message: "error updating parent category",
+    });
+}
+
+    }
+
+
+    // ,removeparentcat
+
+    exports.removeparentcat = async (req, res) => {
+        const parentcat = await parentCatModel.findByIdAndDelete(req.params.id);
+console.log(parentcat);
+
+      try{
+
         res.status(200).json({
-            message : "all parent categories",
-            parentcats,
-        });
-    
+          message: "parent category deleted successfully",
+          parentcat,
+      })
+      }
+       
+      catch(err){
+        console.log(err);
+        res.status(400).json({
+          errmsg:err,
+          message: "error deleting parent category",
+      })
+      }
+      
+        
+
+        
+
+      
+
     }
--- routes/parentcat.js
+++ routes/parentcat.js
@@ -6,7 +6,11 @@ const {auth,adminCheck
 
 } = require("../middleware.js/auth");
 
-const { allParentCats, createParentCat } = require("../controllers/parentcat");
+// remove image from cloudinary
+
+const { remove } = require("../controllers/cloudinary");
+
+const { allParentCats, createParentCat,updateParentCat,removeparentcat } = require("../controllers/parentcat");
 
 
 // create parent category
@@ -16,6 +20,12 @@ router.post("/createParentCat",auth,adminCheck,createParentCat);
 
 // all parent categories
 router.get("/allParentCats",allParentCats);
+
+router.put("/updateParentCat/:id",auth,adminCheck,updateParentCat);
+
+router.post("/removeImage",auth,remove);
+
+router.delete("/removeparentcat/:id",auth,adminCheck,removeparentcat);
 
 
 module.exports = router;
